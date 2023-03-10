8de9440106bcd7e92d06072b523ae15483601b7b
mit-riya <95142933+mit-riya@users.noreply.github.com>
2023-03-10 12:54:29 +0530

    Update welcome_screen.dart

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,19 +1,42 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:flutter/services.dart';
 import 'package:kriti/screens/registration_screen.dart';
 import 'login_sreen.dart';
+// import 'package:remove_background/remove_background.dart';
+import 'dart:typed_data';
+import 'dart:ui' as ui;
 
 class WelcomeScreen extends StatefulWidget {
   static String id='welcome_screen';
   @override
   _WelcomeScreenState createState() => _WelcomeScreenState();
+
 }
 
 class _WelcomeScreenState extends State<WelcomeScreen> {
+  bool isLoaded = false;
+  ui.Image? image;
+  ByteData? pngBytes;
+
   @override
+
+  getUiImage() async {
+    ByteData data = await rootBundle.load('lib/assets/grocery-shopping.png');
+    image = await decodeImageFromList(data.buffer.asUint8List());
+    await getPNG();
+    setState(() {
+      isLoaded = true;
+    });
+  }
+
+  getPNG() async {
+    pngBytes = await image?.toByteData(format: ui.ImageByteFormat.png);
+  }
   void initState() {
     super.initState();
+    getUiImage();
     Firebase.initializeApp().whenComplete(() {
       print("completed");
       setState(() {});
@@ -22,10 +45,7 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('MarketMate'),
-      ),
-      backgroundColor: Colors.white,
+      backgroundColor: Colors.deepPurpleAccent,
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 24.0),
         child: Column(
@@ -34,30 +54,49 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
           children: <Widget>[
             Row(
               children: <Widget>[
-                Container(
-                  // child: Image.asset('assets/images/logo.png'),
-                  height: 60.0,
-                ),
-                SizedBox(
-                  child: DefaultTextStyle(
-                    style: const TextStyle(
-                      color: Colors.black,
-                      fontSize: 45.0,
-                      fontWeight: FontWeight.w900,
-                    ),
-                    child: AnimatedTextKit(
-                      animatedTexts: [TyperAnimatedText('MarketMate')],
-                      // textStyle: TextStyle(
-                      //   fontSize: 45.0,
-                      //   fontWeight: FontWeight.w900,
-                      // ),
-                    ),
+                FloatingActionButton(
+                  backgroundColor: Colors.amberAccent,
+                  onPressed: () {},
+                  child: Icon(
+                    Icons.shopping_cart,
+                    size: 40,
+                    color: Colors.red,
                   ),
+                ),
+                AnimatedTextKit(
+                  animatedTexts: [TyperAnimatedText('  MarketMate',textStyle: TextStyle(
+                    fontSize: 45.0,
+                    fontWeight: FontWeight.bold
+                  ))],
                 ),
               ],
             ),
             SizedBox(
               height: 48.0,
+            ),
+            Container(
+              child:
+                isLoaded
+                    ? Image.memory(Uint8List.view(pngBytes!.buffer))
+                    : const Icon(Icons.image),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 16.0),
+              child: RichText(
+                textAlign: TextAlign.center,
+                text: TextSpan(
+                  children: <TextSpan>[
+                    TextSpan(
+                      text: "Shopping at campus is now easier than ever\n",
+                      style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 30.0),
+                    ),
+                    TextSpan(
+                      text: "Enjoy hassle free shopping\n",
+                      style: TextStyle(color: Colors.white,fontSize: 18.0),
+                    ),
+                  ],
+                ),
+              ),
             ),
             Padding(
               padding: EdgeInsets.symmetric(vertical: 16.0),
@@ -73,6 +112,9 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
                   height: 42.0,
                   child: Text(
                     'Log In',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold
+                    ),
                   ),
                 ),
               ),
@@ -80,7 +122,7 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
             Padding(
               padding: EdgeInsets.symmetric(vertical: 16.0),
               child: Material(
-                color: Colors.blueAccent,
+                color: Colors.lightBlueAccent,
                 borderRadius: BorderRadius.circular(30.0),
                 elevation: 5.0,
                 child: MaterialButton(
@@ -91,6 +133,9 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
                   height: 42.0,
                   child: Text(
                     'Register',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold
+                    ),
                   ),
                 ),
               ),
